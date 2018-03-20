342ef392c5668771af40c482b3f800a58ce3b79d
炒饭 <chaofan@2dfire.com>
2018-03-20 11:39:33 +0800

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # mp-promisify
-小程序接口promise化
+
+小程序接口promise化，`async`/`await`水到渠成
+
+## 使用
+
+```js
+const promisify = require('mp-promisify');
+
+const request = promisify(wx.request);
+const setStorage = promisify.keyDataPromisify(wx.setStorage);
+
+request({
+  url: 'foo/1'
+}).then((result) => {
+  console.log("The result of request foo/1", result);
+}, (e) => {
+  console.log("request had error", e);
+});
+
+setStorage('foo', '123')
+  .then((result) => {
+    console.log("The result of setStorage foo", result);
+  }, (e) => {
+    console.log("setStorage had error", e);
+  });
+```
--- mp-promisify.js
+++ mp-promisify.js
@@ -1,7 +1,7 @@
 /**
  * 接口Promise化
  * @param {Function} mpFunction - function({ any arguments..., function success, function fail})
- * @return {Function}
+ * @return {Function} - function({ any arguments...})
  */
 function promisify (mpFunction) {
   return function promisifyApi (options) {
@@ -14,6 +14,11 @@ function promisify (mpFunction) {
   }
 }
 
+/**
+ * 接口Promise化, 针对key data形式，如setStorage
+ * @param {Function} mpFunction - function({ any arguments..., function success, function fail})
+ * @return {Function} - function(key, data)
+ */
 function keyDataPromisify (mpFunction) {
   return function promisifyApi (key, data) {
     promisify(mpFunction)({ key, data })
